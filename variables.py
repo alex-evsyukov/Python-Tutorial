4412633c67c0629a714c83a91f3a8184be7938da
Alexander Evsyukov <alexander19.evsyukov97@gmail.com>
2023-03-16 17:03:49 +0300

    Variables

---
+++ variables.py
@@ -0,0 +1,22 @@
+Strings
+first_name = "Bro"
+last_name = "Code"
+full_name = first_name + " " + last_name
+print("Hello " + full_name)  # Hello Bro Code
+print(type(full_name))  # <class 'str'>
+
+int
+age = 21
+age += 1
+print("Your age is: " + str(age))  # Your age is: 22
+print(type(age))  # <class 'int'>
+
+float
+height = 250.5
+print("Your height is: " + str(height) + "cm")  # Your height is: 250.5cm
+print(type(height))  # <class 'float'>
+
+boolean
+human = True
+print("Are you a human: " + str(human))  # Are you a human: True
+print(type(human))  # <class 'bool'>
